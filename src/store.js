16e1068fa86c34345fc532230e74846d0ad04396
이해빈 <luckylucy0717@gmail.com>
2019-05-21 23:32:01 +0900

    미들웨어만들기, redux-logger적용, redux-thunk적용

--- src/store.js
+++ src/store.js
@@ -1,6 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
+//import loggerMiddleware from './lib/loggerMiddleware';
 
-const store = createStore(modules)
+import {createLogger} from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
+
+/*
+    미들웨어가 여러개 일 때는 파라미터로 전달하면 된다. applyMiddleware(a,b,c)
+    미들웨어 순서는 여기 전달된 파라미터 순서대로 지정된다.
+*/
+const logger = createLogger()
+const store = createStore(modules, applyMiddleware(logger, ReduxThunk))
 
 export default store;
